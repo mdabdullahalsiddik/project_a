8fb9947c1ddaa37da2cea5b8944a072b869cff11
Md Abdullah Al Siddik <119064797+mdabdullahalsiddik@users.noreply.github.com>
2025-07-31 01:20:29 +0600

    paymet kye

--- lib/app/data/services/payment_key.dart
+++ lib/app/data/services/payment_key.dart
@@ -1,4 +1,6 @@
+///=================STRIPE========================
 library;
 
-const String stripePublishableKey = String.fromEnvironment('STRIPE_PUBLISHABLE_KEY');
-const String stripeSecretKey = String.fromEnvironment('STRIPE_SECRET_KEY');
+final String stripePublishableKey = "pk_test_51RV4ORDENSObmJv7423HHaBNeBjbrVuHAzGp26xnBicJf5pUUbnNhEj2Z1gkdFzsoVCGqtK3GQ7dviGqwmz50Psl003m2dId6G";
+final String stripeSecretKey = "sk_test_51RV4ORDENSObmJv7hzL2FXhdxnFao8LQs5xvsiFfdT2Sc7is3BeGQiFa20pRMmJ4xNCHxZ6WedqLyjbWL4Ueiwwh007MO3ZZEy";
+
