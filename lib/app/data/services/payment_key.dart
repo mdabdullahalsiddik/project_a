92ea464e7c7673efc44765840c962d59bcde4082
Md Abdullah Al Siddik <119064797+mdabdullahalsiddik@users.noreply.github.com>
2025-07-31 01:15:43 +0600

    first commit

--- lib/app/data/services/payment_key.dart
+++ lib/app/data/services/payment_key.dart
@@ -1,6 +1,6 @@
 ///=================STRIPE========================
 library;
 
-final String stripePublishableKey = "pk_test_51RV4ORDENSObmJv7423HHaBNeBjbrVuHAzGp26xnBicJf5pUUbnNhEj2Z1gkdFzsoVCGqtK3GQ7dviGqwmz50Psl003m2dId6G";
-final String stripeSecretKey = "sk_test_51RV4ORDENSObmJv7hzL2FXhdxnFao8LQs5xvsiFfdT2Sc7is3BeGQiFa20pRMmJ4xNCHxZ6WedqLyjbWL4Ueiwwh007MO3ZZEy";
+final String stripePublishableKey = "";
+final String stripeSecretKey = "";
 
